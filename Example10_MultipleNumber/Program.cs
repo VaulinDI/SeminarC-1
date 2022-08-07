d4fffd4695e0b4455eaf40e9708b6761817678e7
VaulinDI <vdi85@mail.ru>
2022-08-07 20:30:33 +0300

    multiplenumber task solved

---
+++ Example10_MultipleNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int x = new Random().Next(1, 10); // Заданное число
+Console.WriteLine("Заданное число: " + x);
+Console.WriteLine("Введите число: ");
+int y = int.Parse(Console.ReadLine());
+// Далее определим, кратно ли введенное число y заданному числу x
+if (y % x == 0) {
+    Console.WriteLine("Введенное число кратно заданному");
+}
+else {    
+    Console.WriteLine("Введенное число не кратно заданному. Остаток от деления равен: " + y % x);
+}
