c1dd8990b82ba20212df30e6981cd85dd81001e9
VaulinDI <vdi85@mail.ru>
2022-08-07 20:04:57 +0300

    deletesecondnumber task solved

---
+++ Example09_DeleteSecondNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿int x = new Random().Next(100, 1000);
+int nlast = x % 10;
+int nsecond = x / 10 % 10;
+int nfirst = x / 100 % 10;
+int nrequired = nfirst * 10 + nlast;
+Console.WriteLine("Введенное число: " + x);
+Console.WriteLine("Полученное число: " + nrequired);
