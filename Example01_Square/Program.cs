5dd1635a14333b66cd4107319fbbafb21bd036cb
VaulinDI <vdi85@mail.ru>
2022-08-07 11:42:56 +0300

    square task solved

---
+++ Example01_Square/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+Console.WriteLine("Число a = " + a);
+Console.WriteLine("Число b = " + b);
+if (a == b * b) {
+    Console.WriteLine("Число " + a + " является квадратом числа " + b);
+}
+else if (b == a * a) {
+    Console.WriteLine("Число " + b + " является квадратом числа " + a);
+}
+else {
+    Console.WriteLine("Число " + a + " не является квадратом числа " + b + " и число " + b + " не является квадратом числа " + a);
+}
