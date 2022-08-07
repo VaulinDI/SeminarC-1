b50cd34e247082d90a1ef131fbaf8bdd330b7605
VaulinDI <vdi85@mail.ru>
2022-08-07 21:05:22 +0300

    evennumber task finished

--- Example04_Evennumber/Program.cs
+++ Example04_Evennumber/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите число: ");
-int x = int.Parse(Console.ReadLine());
+int x = Convert.ToInt32(Console.ReadLine());
 if (x % 2 == 0) {
     Console.WriteLine("Введенное чесло является четным");
 }
