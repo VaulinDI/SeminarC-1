03b0d1a8847bd38d36ac549957653efd276ccf07
VaulinDI <vdi85@mail.ru>
2022-08-07 18:57:28 +0300

    maxnumber task solved

---
+++ Example08_Maxnumber/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = new Random().Next(10, 100);
+int nsecond = x % 10;
+int nfirst = (x - nsecond) / 10;
+Console.WriteLine("Сгенерировано число: " + x);
+Console.WriteLine("Вторая цифра числа: " + nsecond);
+Console.WriteLine("Первая цифра числа: " + nfirst);
+if (nsecond > nfirst) {
+    Console.WriteLine("Наибольшая цифра числа: " + nsecond);
+}
+else {
+    Console.WriteLine("Наибольшая цифра числа: " + nfirst);
+}
