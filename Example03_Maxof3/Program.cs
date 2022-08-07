df378ff18aaf598254e89aa9735021575cd9b51f
VaulinDI <vdi85@mail.ru>
2022-08-07 12:49:32 +0300

    maxofthree task solved

---
+++ Example03_Maxof3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 4;
+int b = 9;
+int c = 2;
+if (a > b && a > c) {
+    Console.WriteLine("Число " + a + " наибольшее из трех");
+}
+else if (b > a && b > c) {
+    Console.WriteLine("Число " + b + " наибольшее из трех");
+}
+else if (c > a && c > b) {
+    Console.WriteLine("Число " + c + " наибольшее из трех");
+}
